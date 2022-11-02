7b8cc3552f97f4563193bb5b214388785080869d
James Taylor <jrtaylor0614@gmail.com>
2022-11-01 19:45:40 -0600

    veriable_change_puts_statments

--- variables.rb
+++ variables.rb
@@ -10,5 +10,21 @@ year = 2022
 account_balance = 11,100.00
 available_balance = 11,00.00
 # Blooleans variables 
-code_is_fun = false
-code_is_easy = true
+code_is_fun = true
+code_is_easy = false
+
+# string Variables change 
+email = "j0614@gmail.com"
+first_name = "Jim"
+last_name = "Randall"
+
+puts email
+puts first_name
+puts last_name
+puts age
+puts month
+puts year
+puts account_balance
+puts available_balance
+puts code_is_easy
+puts code_is_fun
